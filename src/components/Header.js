f8dcbf465683dd6e56b64fb2895019362510fe36
Asadullah Khan <asadullahk15@gmail.com>
2021-02-23 20:16:48 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
     return (
         <div>
-            <h1>Task Tracker By Asadullah Khan</h1>
+            <h1>Task Tracker</h1>
         </div>
     )
 }
