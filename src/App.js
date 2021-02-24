fb3546095b9bbcf7536d11568e13415dace93d8b
Asadullah Khan <asadullahk15@gmail.com>
2021-02-24 17:47:22 +0500

    Updating

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-
+import { useState } from 'react';
 import Header from './components/Header'
 import Task from './components/Task'
 function App() {
- 
+  const [tasks , setTask] = useState([
+    {
+      id : 1,
+      text : 'Moring Walk',
+      day : 'Monday',
+      reminder : true,
+    },
+    {
+      id : 2,
+      text : 'Evening Walk',
+      day : 'Friday',
+      reminder : true,
+    },
+    {
+      id : 3,
+      text : 'Night  Walk',
+      day : 'Saturday',
+      reminder :false,
+    }
+  ])
  return (
     <div className='container'>
       <Header/>
-      <Task/>
+      <Task tasks = {tasks}/>
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,27 +1,7 @@
-import { useState } from 'react';
 
-const Task = () => {
-    const [tasks , setTask] = useState([
-        {
-          id : 1,
-          text : 'Moring Walk',
-          day : 'Monday',
-          reminder : true,
-        },
-        {
-          id : 2,
-          text : 'Evening Walk',
-          day : 'Friday',
-          reminder : true,
-        },
-        {
-          id : 3,
-          text : 'Night  Walk',
-          day : 'Saturday',
-          reminder :false,
-        }
-      ])
-    
+
+const Task = ({tasks}) => {
+ 
     return (
         <>
         {tasks.map((task) => <h3>{task.text}</h3> )}
