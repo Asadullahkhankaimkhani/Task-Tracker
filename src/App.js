c895f88951587f4fa0cef71c9c207d23b4aad0e6
Asadullah Khan <asadullahk15@gmail.com>
2021-02-27 20:39:21 +0500

    Updating

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Header from './components/Header'
 import Task from './components/Task'
+import AddTask from './components/AddTask'
 function App() {
   const [tasks , setTask] = useState([
     {
@@ -22,10 +23,24 @@ function App() {
       reminder :false,
     }
   ])
+
+  // Delete Task
+
+const deleteTask = (id) => {
+  setTask(tasks.filter((task)=> task.id !== id))
+}
+// toggle Rememider
+const toggleReminder = (id) => {
+  setTask(tasks.map(task => 
+    task.id === id ? {...task,reminder: !task.reminder} : task))
+}
  return (
     <div className='container'>
       <Header/>
-      <Task tasks = {tasks}/>
+      <AddTask />
+     {tasks.length > 0 ?<Task tasks = {tasks} 
+     onDelete = {deleteTask} onToggle = {toggleReminder}/>: 'No  Tasks to show'}
+     
     </div>
   );
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,12 @@
-const Task = ({tasks}) => {
+import SingleTask from './SingleTask'
+const Task = ({tasks,onDelete,onToggle}) => {
  
     return (
-        <div>
-        {tasks.map((task) => <h3>{task.text}</h3> )}
-        <div/>
+        <>
+        {tasks.map((task) =>(  
+        <SingleTask key = {task.id} task = {task} onDelete = {onDelete} onToggle = {onToggle}/> 
+        ))}
+        </>
     )
 }
 
